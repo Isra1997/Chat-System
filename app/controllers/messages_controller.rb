a2a70f3b77c0977d609d18260902da6eab176665
Isra1997 <israragheb@gmail.com>
2023-12-11 21:45:58 +0200

    fix: endpoint validations

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,19 +3,38 @@ class MessagesController < ApplicationController
     
         # GET /application/:application_token/chats/:chat_number/message
         def index
-            p params
-            @messages = Message.all
-            render json: @messages
+            @chat = Chat.where(token: params[:application_token], number: params[:chat_number])[0]
+            if @chat.present?
+                @messages = Message.where(
+                    chat_id: @chat.id)
+            else
+                render json: "No chats for application token: "+ params[:application_token] + " and chat number: " + params[:chat_number] , status: not_found
+            end
+            if @messages.present?
+                render json: @messages
+            else
+                render json: "No messages for application token: "+ params[:application_token] + " and chat number: " + params[:chat_number], status:  :not_found
+            end
         end
     
         # GET /application/:application_token/chats/:chat_number/messages/:message_number 
         def show
-            render json: @message
+            if @message.present?
+                render json: @message
+            else
+               render json: "No messages for application token: "+ params[:application_token] +
+                " and chat number: " + params[:chat_number] + " and message number: " + params[:message_number] , status:  :not_found
+            end
         end
     
         # DELETE /application/:application_token/chats/:chat_number/messages/:message_number
         def destroy
-            Message.destroy(@message[0].id)
+            if @message.present?
+                Message.destroy(@message[0].id)
+            else
+                render json: "No messages for application token: "+ params[:application_token] +
+                " and chat number: " + params[:chat_number] + " and message number: " + params[:message_number] , status:  :not_found
+            end
         end
     
         # POST /application/:application_token/chats/:chat_number/messages
@@ -29,7 +48,7 @@ class MessagesController < ApplicationController
                     render json: @message.errors, status: :unprocessable_entity
                 end
             else
-                render json: "Chat not found", status:  :not_found
+                render json: "No chats for application token: " + params[:application_token] + " and chat number: " + params[:chat_number] , status: not_found
             end
         end
 
@@ -37,8 +56,12 @@ class MessagesController < ApplicationController
         def search
             Message.reindex
             query = params[:query]
-            results = Message.search(params[:query])
-            render json: results
+            @messages = Message.search(params[:query])
+            if @messages.present?
+                render json: @messages
+            else
+                render json: "No results found" , status: :not_found
+            end
         end
     
         # PATCH/PUT /application/:application_token/chats/:chat_number/messages/:message_number
@@ -56,9 +79,11 @@ class MessagesController < ApplicationController
         # here we set the message according to: application_token, chat_number
         def set_message
             @chat = Chat.where(token: params[:application_token], number: params[:chat_number])[0]
-            @message = Message.where(
-                chat_id: @chat.id, 
-                message_number: params[:message_number])[0]
+            if @chat.present?
+                @message = Message.where(
+                    chat_id: @chat.id, 
+                    message_number: params[:message_number])[0]
+            end
         end
     
 end
